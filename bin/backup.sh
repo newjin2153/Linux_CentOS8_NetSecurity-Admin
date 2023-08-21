bb5514608f4d6da6a15cb5c38c008ca571f23ab5
newjin2153 <newjin2153@gmail.com>
2023-08-21 15:13:40 +0900

    2nd commit

---
+++ bin/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#cd /home
+#tar cvzf /backup/home_$(date +%m%d_%H%M%S).tar.gz .
+
+
+#!/bin/bash
+
+cd /home
+tar cvzf /backup/backup_$(date +'%m%d_%H%M%S').tar.gz .
+
+
+
---
+++ bin/userdel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat /root/bin/passwd.txt | while read UNAME UPASS
+do
+	# echo "$UNAME : $UPASS"
+	userdel -r $UNAME
+	echo "[  OK  ] $UNAME deleted."
+done
